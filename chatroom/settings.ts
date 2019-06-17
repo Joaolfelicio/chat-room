4ca73714d90e9d4fc9642467c2bc0a6b32e428df
Joao Felicio <joaolfelicio@gmail.com>
2019-06-17 18:58:12 +0200

    fixed bug

--- chatroom/settings.ts
+++
@@ -1,4 +0,0 @@
-export const linkGetMessage:string = "http://ajax.i-marty.eu/chatMessages"
-export const linkAddMessage:string = "http://ajax.i-marty.eu/addMessage"
-export const linkCreateUser:string = "http://ajax.i-marty.eu/addUser"
-export const linkLoginUser:string = "http://ajax.i-marty.eu/checkUser"
